98c661187667049b6327edbfb5bff1a473568a2a
NoackRob <NoackRob@BBWS0-F3SFR53>
2022-01-06 14:15:00 +0100

    hallo

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Buecherei.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Buecherei.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bac67423-4be0-4c26-a29e-cb85b3357a30")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PruefungenTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using Buecherei.Properties;
+using Moq;
+using System;
+
+namespace Buecherei.UnitTests
+{
+    [TestFixture]
+    public class PruefungenTests
+    {
+        [Test]
+        public void EingabeZahlPruefung_ZahlWirdEingegeben_ReturnTrue()
+        {
+            var result = Pruefungen.EingabeZahlPruefung("2");
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        [TestCase("a")]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void EingabeZahlPruefung_KeineZahlWirdEingegeben_ReturnFalse(string eingabe)
+        {
+            var result = Pruefungen.EingabeZahlPruefung(eingabe);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void EingabeZahlIstOption_EingabeKleinerAls1_ReturnFalse()
+        {
+            var result = Pruefungen.EingabeZahlIstOption(0, 3);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void EingabeZahlIstOption_EingabeGroeßerAlsMoeglichkeiten_ReturnFalse()
+        {
+            var result = Pruefungen.EingabeZahlIstOption(4, 3);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void EingabeZahlIstOption_EingabekleinerAlsMoeglichkeitenUndGroeßerAlsNull_ReturnTrue()
+        {
+            var result = Pruefungen.EingabeZahlIstOption(1, 3);
+
+            Assert.That(result, Is.True);
+        }
+
+
+    }
+}
